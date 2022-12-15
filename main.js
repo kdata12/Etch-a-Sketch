bdde4567b45d38427df0cd2ca6a33f9c11e01a3a
kevin <kevinvong0129@gmail.com>
2022-12-15 12:55:34 -0700

    added overflow, everything is working fine

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 const container = document.querySelector(".container");
 let numSquares = parseInt(container.classList[1]);
 numSquares = 16;
-const containerSize = 585;
-const pixel = containerSize/numSquares;
+const containerSize = 600;
+let pixel = containerSize/numSquares;
+pixel;
 //rows
-for(let i = 0; i < 16; i++) {
+for(let i = 0; i < numSquares; i++) {
     let column = document.createElement('div')
     column.setAttribute('class', `column-${i}`);
     column.style.cssText = ` border: 0.5px solid black; 
-                                height: ${pixel}; 
+                                height: ${pixel}px; 
                                 width: 600px; 
                                 margin: 0; display: inline;
                                 clear:both;
-                                display:flex`;
+                                display:flex
+                                `;
     container.appendChild(column);
 
     const row1 = document.querySelector(`div.column-${i}`);
-    for(let i = 0; i < 16; i++) {
+    for(let i = 0; i < numSquares; i++) {
         let divSquare2 = document.createElement('div')
         divSquare2.setAttribute('class', `box`);
         divSquare2.style.cssText = `position: relative;border: 0.2px solid black; 
@@ -40,6 +42,12 @@ allBoxes.forEach(box => box.addEventListener('mouseover', switchColor))
 function switchColor () {
     this.setAttribute('class', 'box hover')
 }
+
+function clearBoard() {
+    allBoxes.forEach(box => box.setAttribute('class', 'box'));
+}
+
+
 
 
 
